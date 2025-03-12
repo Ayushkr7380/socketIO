900a7e5b93d2129a8668caa5a716c19cb7c1bdb3
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-03-12 13:58:01 +0530

    username feature added

--- Basic/index.js
+++ Basic/index.js
@@ -10,15 +10,27 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+const users = {};
 //socket.io
 io.on("connection",(anything)=>{
+
+    //setUserName , received from frontend to server
+    anything.on("setUserName",(userName)=>{
+        users[anything.id] = userName;
+        io.emit("userList",users);
+    });
+
+
     //Receive message to server from frontend
     anything.on("user-message",(message)=>{
         // console.log(`${message} from ${anything.id}`);
 
-        
+
         //Sends the received message from user-message(frontend) to all users
-        io.emit("message-to-all",message); 
+        io.emit("message-to-all",{
+            user : users[anything.id] || "Anonymous",
+            text : message,
+        }); 
     })
 });
 
