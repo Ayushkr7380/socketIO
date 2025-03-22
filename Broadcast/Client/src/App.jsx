b817773eb023ff064aa6d768672928ed659b6cc1
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-03-22 21:25:44 +0530

    minor changes

--- Broadcast/Client/src/App.jsx
+++ Broadcast/Client/src/App.jsx
@@ -4,31 +4,50 @@ import { io } from 'socket.io-client';
 const App = () => {
   const socket = useMemo(()=> 
     io("http://localhost:8000/")
-  );
+  ,[]);
 
-  const [messageFromServer , setMessageFromServer] = useState("")
+
+  const [messageFromServer , setMessageFromServer] = useState("");
+  const [inputMessage,setInputMessage] = useState("");
 
   useEffect(()=>{
     socket.on("connect",()=>{
       console.log('Connected',socket.id);
     });
 
-    socket.emit("message","kya haal hai");
+    //socket.emit("message","kya haal hai");
     
     socket.on("message-to-all",(data)=>{
       setMessageFromServer(data);
-    })
+    });
 
+    return ()=>{
+      socket.disconnect();
+    }
   },[])
+
+  const inputHandleChange =(e)=>{
+    setInputMessage(e.target.value);
+  }
+
+  const handleSubmit =()=>{
+    if(socket && inputMessage){
+      socket.emit("message",inputMessage);
+      setInputMessage("");
+    }
+  }
 
   return (
     <>
         <div>
             <div>
-              
-              <input type="text" />
+              <input 
+                type="text"
+                onChange={inputHandleChange}
+                value={inputMessage}
+              />
+              <button onClick={handleSubmit} >Send Message</button>
             </div>
-
              {messageFromServer && <p>{messageFromServer}</p>}
           
         </div>
