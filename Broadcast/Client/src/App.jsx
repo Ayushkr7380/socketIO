b212b2941c67650348dc3032429e344762313cc7
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-03-25 22:57:12 +0530

    comment added

--- Broadcast/Client/src/App.jsx
+++ Broadcast/Client/src/App.jsx
@@ -10,17 +10,23 @@ const App = () => {
   const [joinedUser,setJoinedUser] = useState([]);
 
   useEffect(()=>{
+    //Socket connection
     socket.on("connect",()=>{
       console.log('Connected',socket.id);
+      //set socket id in useState
       setSocketId(socket.id);
+
+      //Send socket id to server side
       socket.emit("userId",socket.id);
     });
     
+    //Receive who joined the chat from server to frontend side 
     socket.on("userId",(id)=>{
       console.log(`${id} joined the chat.`);
       setJoinedUser((prev)=>[...prev,id]);
     });
 
+    //Receive messages from server to frontend
     socket.on("message-to-all",({socketId,inputMessage})=>{    
       setMessageFromServer((prev)=>[...prev,{socketId,inputMessage}]);
     });
@@ -30,12 +36,15 @@ const App = () => {
     }
   },[]);
 
+
   const inputHandleChange =(e)=>{
     setInputMessage(e.target.value);
   }
 
+  //handleSubmit function to send message from frontend to backend
   const handleSubmit =()=>{
     if(socket && inputMessage){
+      //Socket.emit sends message to backend from frontend
       socket.emit("message",{
         socketId,
         inputMessage
@@ -44,7 +53,7 @@ const App = () => {
     }
   }
 
-  console.log("----",messageFromServer);
+  // console.log("----",messageFromServer);
   return (
     <>
         <div>
